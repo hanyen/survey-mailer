bd0d3785a596cf789124e7df8f804224203d0be1
hanyen <me@hanyen.com>
2017-09-18 16:56:54 -0700

    Setup mongodb for user auth

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users'); //pull a model out of mongoose (single argument)
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +13,18 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
+      // console.log('access token', accessToken);
+      // console.log('refresh token', refreshToken);
+      // console.log('profile', profile);
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            //we already have a record with the given profile Id
+          } else {
+            //we don't have a record with this id, make a new record
+            new User({ googleId: profile.id }).save();
+          }
+        });
     }
   )
 );
