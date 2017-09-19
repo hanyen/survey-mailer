23720b7db161e318e4b0d3eb566a833ba673c890
hanyen <me@hanyen.com>
2017-09-19 10:27:40 -0700

    setting up dev and prod environment

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+//prod.js - do commit this
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+//914445141730-q0ns64370l866il1asbt4u01cpp84sdm.apps.googleusercontent.com
+//w4euvuzwpZB5HaDkIZPmsBXm
+
