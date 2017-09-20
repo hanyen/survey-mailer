2f1bd9d52db636b7be4180ffd396a4cf2a342d7b
hanyen <me@hanyen.com>
2017-09-20 15:36:09 -0700

    refactor passport.js using es6 async

--- services/passport.js
+++ services/passport.js
@@ -24,21 +24,16 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      // console.log('access token', accessToken);
-      // console.log('refresh token', refreshToken);
-      // console.log('profile', profile);
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          //we already have a record with the given profile Id
-          done(null, existingUser);
-        } else {
-          //we don't have a record with this id, make a new record
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        //we already have a record with the given profile Id
+        return done(null, existingUser);
+      }
+      //we don't have a record with this id, make a new record
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
