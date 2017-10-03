85b6a3311957aa033642a224b241e5a753afecac
hanyen <me@hanyen.com>
2017-10-03 12:30:01 -0700

    setup sendgrid key

--- config/prod.js
+++ config/prod.js
@@ -5,7 +5,8 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY
 };
 
 //914445141730-q0ns64370l866il1asbt4u01cpp84sdm.apps.googleusercontent.com
